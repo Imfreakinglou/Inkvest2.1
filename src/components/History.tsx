c1c203a2fd14d2c0678316d7cb3662183f7bb7bb
Imfreakinglou <edd@culturebrandcreative.co>
2025-05-10 09:42:09 -0500

    Create History component

---
+++ src/components/History.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const History: React.FC = () => {
+  return (
+    <div className="bg-white shadow rounded-lg p-6">
+      <h2 className="text-2xl font-bold mb-4">History</h2>
+      <div className="space-y-4">
+        {/* History content will be implemented later */}
+        <p className="text-gray-600">No history items to display.</p>
+      </div>
+    </div>
+  );
+};
+
+export default History;
