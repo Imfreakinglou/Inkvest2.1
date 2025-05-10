d02eb04062391bd93569e8d2e767a010c7626cee
Imfreakinglou <edd@culturebrandcreative.co>
2025-05-10 09:42:43 -0500

    Create CameraComponent file

---
+++ src/components/CameraComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CameraComponent: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Camera Upload</h2>
+      <p className="text-gray-600">Camera functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default CameraComponent;
